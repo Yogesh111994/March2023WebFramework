8886b61bb94e15828d1a6016e31c750d0aa7922d
biradaryogesh11 <112967452+biradaryogesh11@users.noreply.github.com>
2024-02-29 23:27:17 +0530

    In cart page by locator and action method are added

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,6 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
-	int i = 10;
+By name = By.id("yogesh");
+
+public void getname() {
+	System.out.println("yogesh");
+}
 }
